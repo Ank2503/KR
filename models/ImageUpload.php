bb32ca5d3f588e62cf758b7d4b6f5566bb1c5d48
Alexander Psaryov <aleksua18@gmail.com>
2019-12-27 18:05:58 +0200

    CRUD

---
+++ models/ImageUpload.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *  * Created by Andrey on 08.11.2019.
+ */
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\web\UploadedFile;
+
+
+class ImageUpload extends Model
+{
+    public $image;
+
+    public function rules()
+    {
+        return [
+            [['image'],'required'],
+            [['image'],'file','extensions'=>'jpg,png']
+            ];
+    }
+
+    public function uploadFile(UploadedFile $file,$currentImage)
+    {
+        $this->image=$file;
+        if($this->validate()) {
+            $this->removeImage($currentImage);
+            return $this->saveImage();
+        }
+    }
+
+    public function getFolder()
+    {
+        return Yii::getAlias('@web').'uploads/';
+    }
+
+    public function Encrypt()
+    {
+        return strtolower(md5(uniqid($this->image->baseName)) . '.' . $this->image->extension);
+    }
+
+    public function removeImage($currentImage)
+    {
+        if($this->isExists($currentImage))
+        {
+            unlink($this->getFolder() . $currentImage);
+        }
+
+    }
+
+    public function isExists($currentImage)
+    {
+        if (!empty($currentImage) && $currentImage != null) {
+            return file_exists($this->getFolder() . $currentImage);
+        }
+    }
+
+    public function saveImage()
+    {
+        $filename = $this->Encrypt();
+
+        $this->image->saveAs(Yii::getAlias('@web') . 'uploads/' . $filename);
+
+        return $filename;
+    }
+
+}
--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -2,12 +2,14 @@
 
 namespace app\modules\admin\controllers;
 
+use app\models\ImageUpload;
 use Yii;
 use app\models\Article;
 use app\models\ArticleSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
 
 /**
  * ArticleController implements the CRUD actions for Article model.
@@ -116,6 +118,23 @@ class ArticleController extends Controller
      * @return Article the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
+
+
+    public function actionSetImage($id)
+    {
+        $model=new ImageUpload;
+        if (Yii::$app->request->isPost)
+            {
+                $article = $this->findModel($id);
+                $file = UploadedFile::getInstance($model,'image');
+                if($article->saveImage($model->uploadFile($file,$article->image)));
+                {
+                    return $this->redirect(['view','id'=>$article->id]);
+                }
+
+            }
+       // return $this->render('image',['model'=>$model]);
+    }
     protected function findModel($id)
     {
         if (($model = Article::findOne($id)) !== null) {
